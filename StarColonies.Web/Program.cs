cb7354b237bc9dcbd4c53c5a45b72a226403d48e
Nicolas Hendrikx <n.hendrikx@helmo.be>
2025-03-10 14:28:52 +0100

    Bug fix: enregistrer le middleware

--- StarColonies.Web/Program.cs
+++ StarColonies.Web/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<ReverseProxyLinksMiddleware>();
 
 var app = builder.Build();
 
