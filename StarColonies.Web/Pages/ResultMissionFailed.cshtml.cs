c60dfb03cafc71a82ed43d230332dea237b5a3a5
ori <clesse.oriane1@gmail.com>
2025-03-29 19:10:05 +0100

    Ajout de mission ratée

---
+++ StarColonies.Web/Pages/ResultMissionFailed.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarColonies.Web.Pages;
+
+public class ResultMissionFailed : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
