b2635ec278c7f0a2ea088acad30c691e02aa7c6b
ori <clesse.oriane1@gmail.com>
2025-04-20 18:51:07 +0200

    Creation page profil pour l'instant

---
+++ StarColonies.Web/Pages/Profil.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StarColonies.Domains;
+
+namespace StarColonies.Web.Pages;
+
+public class Profil : PageModel
+{
+    private readonly IColonRepository _repository;
+    private readonly ILogger<ConsultMission> _logger;
+
+    public Colon User { get; private set; }
+
+    public Profil(IColonRepository colonRepository, ILogger<ConsultMission> logger)
+    {
+        _repository = colonRepository;
+        _logger = logger;
+    }
+
+
+    public async Task<IActionResult> OnGet()
+    {
+        try
+        {
+            User = await _repository.GetColonByIdAsync("testUser");
+        } 
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Comptoir");
+        }
+        return Page();
+        
+    }
+
+    public void OnPost(string slug)
+    {
+        
+    }
+}
