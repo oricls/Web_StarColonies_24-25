c232c92da660759b7dbd66dadb83678f237e3485
ori <clesse.oriane1@gmail.com>
2025-04-21 11:58:20 +0200

    Ajout des formulaires + maj view

--- StarColonies.Web/Pages/Profil.cshtml.cs
+++ StarColonies.Web/Pages/Profil.cshtml.cs
@@ -4,6 +4,15 @@ using StarColonies.Domains;
 
 namespace StarColonies.Web.Pages;
 
+public class UpdateProfilViewModel
+{
+    public string Courriel { get; set; }
+    public string NomDeColon { get; set; }
+    public DateTime DateDeNaissance { get; set; }
+    public string NouveauMotDePasse { get; set; }
+    public string ConfirmationMotDePasse { get; set; }
+}
+
 public class Profil : PageModel
 {
     private readonly IColonRepository _repository;
@@ -11,6 +20,9 @@ public class Profil : PageModel
 
     public Colon User { get; private set; }
 
+    [BindProperty]
+    public UpdateProfilViewModel UpdateProfil { get; set; }
+    
     public Profil(IColonRepository colonRepository, ILogger<ConsultMission> logger)
     {
         _repository = colonRepository;
@@ -32,8 +44,39 @@ public class Profil : PageModel
         
     }
 
-    public void OnPost(string slug)
+    public async Task<IActionResult> OnPost(string slug)
     {
-        
+        // faire vérifs
+        if (InputIsEmpty(UpdateProfil.NouveauMotDePasse) || InputIsEmpty(UpdateProfil.ConfirmationMotDePasse) ||
+            InputIsEmpty(UpdateProfil.Courriel) || InputIsEmpty(UpdateProfil.NomDeColon))
+        {
+            throw new Exception("Les champs doivent etre remplis");
+        }
+
+        if (UpdateProfil.NouveauMotDePasse != UpdateProfil.ConfirmationMotDePasse)
+        {
+            throw new Exception("Le mot de passe ne convient pas");
+        }
+
+        try
+        {
+            var colonUpdate = await _repository.GetColonByIdAsync("testUser");
+            colonUpdate.Name = UpdateProfil.NomDeColon;
+            colonUpdate.Email = UpdateProfil.Courriel;
+            colonUpdate.DateBirth = UpdateProfil.DateDeNaissance;
+            colonUpdate.Password = UpdateProfil.NouveauMotDePasse;
+
+            await _repository.UpdateColonAsync(colonUpdate);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Profile");
+        }
+        return Page();
+    }
+
+    private bool InputIsEmpty(string intput)
+    {
+        return string.IsNullOrWhiteSpace(intput);
     }
 }
