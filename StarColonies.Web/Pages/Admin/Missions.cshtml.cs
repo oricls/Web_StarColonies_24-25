070fee7a2bc9e7a2028fc6120d37ca28621e0059
Altad <a.tadino@student.helmo.be>
2025-04-25 23:13:33 +0200

    pannel admin + add mission not finish

---
+++ StarColonies.Web/Pages/Admin/Missions.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarColonies.Web.Pages.Admin;
+
+public class Missions : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ StarColonies.Web/Pages/Admin/Users.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarColonies.Web.Pages.Admin;
+
+public class Users : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
