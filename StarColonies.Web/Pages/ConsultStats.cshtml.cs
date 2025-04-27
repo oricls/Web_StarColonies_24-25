5eee1941d63c3284517f19f6430ff7636d991c53
ori <clesse.oriane1@gmail.com>
2025-04-27 12:39:05 +0200

    Maj graph + navbar + ajout importation

--- StarColonies.Web/Pages/ConsultStats.cshtml.cs
+++ StarColonies.Web/Pages/ConsultStats.cshtml.cs
@@ -14,11 +14,20 @@ public class StatPoint
     public double Strength { get; set; }
 }
 
-public class ConsultStatsModel(
-    UserManager<Infrastructures.Entities.Colon> userManager,
-    ITeamRepository teamRepository,
-    IMissionRepository missionRepository,
-    ILogger<ConsultStatsModel> logger)
+public class VictoryData
+{
+    public int TotalSuccess { get; private set; } = 0;
+    public int TotalFailure { get; private set; } = 0;
+    
+    public void AddPoint(int success, int failure)
+    {
+        TotalSuccess += success;
+        TotalFailure += failure;
+    }
+}
+
+public class ConsultStatsModel(UserManager<Infrastructures.Entities.Colon> userManager, ITeamRepository teamRepository,
+    IMissionRepository missionRepository, ILogger<ConsultStatsModel> logger)
     : PageModel
 {
     public IReadOnlyList<Team> UserTeams { get; private set; } = new List<Team>();
@@ -26,10 +35,10 @@ public class ConsultStatsModel(
     [BindProperty(SupportsGet = true)]
     [Display(Name = "Équipe")]
     public int SelectedTeamId { get; set; }
-    public int TotalSuccess { get; private set; } = 0;
-    public int TotalFailure { get; private set; } = 0;
+    
 
     public List<StatPoint> StatsEvolution { get; private set; } = new();
+    public VictoryData StatVictoryData { get; private set; } = new();
 
     private async Task<Infrastructures.Entities.Colon> GetCurrentUserAsync()
     {
@@ -61,10 +70,12 @@ public class ConsultStatsModel(
                         Strength = result.IssueStrength,
                         Endurance = result.IssueEndurance
                     });
-
-                    if (result.IsSuccess) TotalSuccess++;
-                    else TotalFailure++;
                 }
+
+                int successes = resultMission.Count(m => m.IsSuccess);
+                int failures = resultMission.Count(m => !m.IsSuccess);
+                StatVictoryData.AddPoint(successes, failures);
+
             }
         }
 
